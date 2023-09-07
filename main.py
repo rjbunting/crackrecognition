f1499ada3329cbcb4870aaacbe06da105cbc0bd2
RhysBunting <103450624+rjbunting@users.noreply.github.com>
2023-09-07 17:49:45 +0200

    Updated version with output files

--- main.py
+++ main.py
@@ -11,19 +11,21 @@ def getcrack(picture):
 
     img_smooth = cv.bilateralFilter(img_crop, 5, 25, 25)
 
-    ret, img_proc = cv.threshold(img_smooth, 75, 255, cv.THRESH_BINARY)
+    img_proc = cv.threshold(img_smooth, 75, 255, cv.THRESH_BINARY)[1]
 
     pixels = np.prod(img_proc.shape)
     crackpixels = list(np.unique(img_proc, return_counts=True)[1])[0]
-    percentcrack = ( crackpixels / pixels ) * 100
+    percentcrack = (crackpixels / pixels) * 100
+
+    cv.imwrite('./output/' + picture.removesuffix('.tif').removeprefix('./input/') + '_proc.tif', img_proc)
 
     return percentcrack
 
 
 def main():
-    images = os.listdir(f'./semimages/.')
+    images = os.listdir(f'input/.')
     for image in images:
-        percentcrack = getcrack('./semimages/' + image)
+        percentcrack = getcrack('./input/' + image)
         print(f'{image} = {percentcrack}')
 
 if __name__ == '__main__':
